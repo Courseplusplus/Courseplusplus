338bd89774db0895820b68114c023b4b0b7321b1
zhangxinru <zxr@xinru.me>
2016-07-05 10:50:52 +0800

    some changes

--- teacher_server/server.js
+++ teacher_server/server.js
@@ -40,6 +40,7 @@ app.use(function (req, res, next) {
     err.status = 404;
     next(err);
 });
+app.use(express.static(path.join(__dirname,'public')));
 
 // development error handler
 // will print stacktrace
