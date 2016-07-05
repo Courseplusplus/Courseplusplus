c3b8a8b0ba61ff452613960cc451d762a6595a5a
王肇一 <manjuulden@hotmail.com>
2016-07-05 15:11:26 +0800

    OK

--- student_server/controllers/index.js
+++ student_server/controllers/index.js
@@ -6,6 +6,7 @@
 module.exports = {
     user: require('./users/index'),
     groups: require('./groups/index'),
+    resource:require('./resource/index'),
     test: function (req, res) {
         res.json({"message": "hello world"});
     }
