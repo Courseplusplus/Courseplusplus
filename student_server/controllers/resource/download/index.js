9d9f82a2a037f159ff2bc1caf20a513733ab8ba6
王肇一 <manjuulden@hotmail.com>
2016-07-06 10:24:36 +0800

    Student download resources

---
+++ student_server/controllers/resource/download/index.js
@@ -0,0 +1,10 @@
+/**
+ * Created by wangzhaoyi on 16/7/6.
+ */
+var util = require('util');
+var fs   = require('fs-extra');
+
+exports.download = function (req,res,next) {
+    var Resource = global.db.models.resource;
+
+};
--- student_server/controllers/resource/index.js
+++ student_server/controllers/resource/index.js
@@ -32,37 +32,4 @@ exports.create = function (req, res, next) {
     });
 };
 
-exports.update = function(req,res,next){
-    var Resource = global.db.models.resource;
-    Resource.find({where:{resource_id:req.body.resource_id}}).then(function (resource) {
-        if(resource) {
-            var updateParams = {
-                resource_type: req.body.resource_type,
-                course_id: req.body.course_id,
-                lesson: req.body.lesson,
-                resource_name: req.body.resource_name,
-                file_path: req.body.file_path
-            };
-            for (var key in updatedParams) {
-                if (updateParams[key]) {
-                    Resource[key] = updateParams[key];
-                }
-            }
-            return Resource.save();
-        }
-        else {
-            next(new Errors.errors_404.UserNotFoundError("No such Resource"));
-        }
-    }).then(function(refreshed_resource){
-        res.json(ResultConstructor.success({
-            resource_id:Resource.resource_id,
-            resource_type:Resource.resource_type,
-            course_id:Resource.course_id,
-            lesson:Resource.lesson,
-            resource_name:Resource.resource_name,
-            file_path:Resource.file_path
-        }));
-    });
-};
-
-exports.upload = require('./upload/index');
+exports.upload = require('./download/index');
