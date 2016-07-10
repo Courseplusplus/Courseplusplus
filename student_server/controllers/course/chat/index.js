4ddc34db4ac53320c49092cc29144d406cb1ab86
peter <zhufengdaaa@gmail.com>
2016-07-10 19:16:42 +0800

    fix assignment bug

---
+++ student_server/controllers/course/chat/index.js
@@ -0,0 +1,6 @@
+/**
+ * Created by peter on 7/10/16.
+ */
+exports.index = function(req,res){
+
+};
---
+++ student_server/routers/course/chat/index.js
@@ -0,0 +1,14 @@
+/**
+ * Created by peter on 7/10/16.
+ */
+var express = require('express');
+var chat_controller = require('../../../controllers').course.chat;
+var authenticator = require('../../../middlewares/authentication');
+
+var router = express.Router({
+	mergeParams: true
+});
+
+//router.get('/', chat_controller.index);
+
+module.exports = router;
