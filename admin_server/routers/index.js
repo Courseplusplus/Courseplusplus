aa16dc3b0af62a42342773a6d033e3e947cfb6e0
王肇一 <manjuulden@hotmail.com>
2016-07-11 11:11:54 +0800

    admin_server index url changed

--- admin_server/routers/index.js
+++ admin_server/routers/index.js
@@ -12,7 +12,7 @@ var request_data_logger = require('../middlewares').request_data_logger;
 
 
 router.use(request_data_logger);
-router.get('/index',controller.index);
+router.get('/',controller.index);
 router.use('/course',course_router);
 router.use('/teacher',teacher_router);
 router.use('/student',student_router);
