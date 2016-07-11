f32ff56b6195ad419fc6dfe9868e764e202311af
王肇一 <manjuulden@hotmail.com>
2016-07-11 16:29:33 +0800

    close course

--- admin_server/routers/course/index.js
+++ admin_server/routers/course/index.js
@@ -13,6 +13,8 @@ router.post('/',controller.courses.import);
 
 router.get('/:course_id',controller.courses.show);
 
+router.get('/:course_id/close',controller.courses.close);
+
 router.post('/:course_id/teacher',controller.courses.import_teacher);
 router.post('/:course_id/student',controller.courses.import_student);
 
